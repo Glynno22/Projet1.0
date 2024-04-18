fecd7dd430a48a55e2b7de3709098beebef0e5fd
DIMBECK ROSINE VALENCIA <touyelaurene@gmail.com>
2024-04-18 13:52:52 +0100

    modif appart

--- APPARTEMENT.php
+++ APPARTEMENT.php
@@ -14,19 +14,52 @@ function insertion($id_app ,$name, $desc , $price , $img){
    echo "rosine";
 
    //modification
-   function modification( $x , $y , $z , $w){
+   
+   function update($id){
     try{
-        $database = new PDO('mysql:host=localhost;dbname=GESTION_IMMOBILIERS' , 'root' , '');
-        $sql=$database->prepare("UPDATE APPARTEMENT SET name=? , description = ?, price_per_night=? , image=? WHERE id_app = 100");
-        $sql->execute(array($x , $y, $z,$w));
-    }
-    catch(Exception $e){
-        die('rosine:)' .$e->getMessage());
-      }
-    }
-      modification("PHILLIWEST" , "5 etoiles" , 78000, "philli.png");
-      
+       $db= new PDO('mysql:host=localhost;dbname=GESTION_IMMOBILIERS', 'root','');
 
+      // $db = new PDO('mysql:host=localhost ; dbname= GESTION_IMMOBILIERS', 'root','');
+      // exit;
+      //recuperation de l'appart correspondant a l'id
+      $sql="SELECT * FROM APPARTEMENT WHERE id_app=:id";
+      $res = $db->prepare($sql);
+      $res->bindParam(':id', $id);
+      $res->execute();
+      $row = $res->fetch();
+      // print_r($row);
+      // exit;
+      if($row){
+        echo "l'appart existe <br>";
+        //modification des champs
+        $nom = "valence";
+        $desc="45 etoiles";
+        $prix = 1444900;
+        $image = "100.pmj";
+        //mettre a jour 
+        $sql="UPDATE APPARTEMENT SET  name=:nom , description=:descrip , price_per_night=:prix , image=:im WHERE id_app =:id";
+        $res=$db->prepare($sql);
+        // UPDATE APPARTEMENT SET  name=:nom , description=:descrip , price_per_night=:prix , image=:im WHERE id_app =4
+        
+        $res->bindParam(':nom' , $nom);
+        $res->bindParam(':descrip', $desc);
+        $res->bindParam(':prix' , $prix);
+        $res->bindParam(':im' , $img);
+        $res->bindParam(':id', $id, PDO::PARAM_INT) ;
+        $res->execute();
+        echo "modif success";
+      }else{
+        echo "n'existe pas";
+        
+      }
+    }catch (Exception $e){
+      echo "ereur".$e->getMessage();
+    }
+    exit;
+
+      }
+      update(46);
+    
 //suppression
       function suppression(){
         try{
